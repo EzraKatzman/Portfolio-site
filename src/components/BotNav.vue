b68048f440668877eb9fe1f9811bb44c077f6caf
Ezra Katzman <47403054+EzraKatzman@users.noreply.github.com>
2020-12-24 13:44:07 -0500

    updated link

--- src/components/BotNav.vue
+++ src/components/BotNav.vue
@@ -15,7 +15,7 @@
                 <a href="mailto:ezkatzman@gmail.com"><svg viewBox="0 0 20 20" fill="currentColor" 
                     class="inline w-5 h-5 xl:w-6 xl:h-6 md:mx-1 text-rose-600 hover:text-gray-100">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg></a>
-                <a href="https://github.com/EzraKatzman/WebsiteV2" target="_blank"><svg viewBox="0 0 20 20" fill="currentColor" 
+                <a href="https://github.com/EzraKatzman/Portfolio-site" target="_blank"><svg viewBox="0 0 20 20" fill="currentColor" 
                     class="inline w-5 h-5 xl:w-6 xl:h-6 md:mx-1 text-rose-800 hover:text-gray-100">
                     <path d="M5.719 14.75a.997.997 0 01-.664-.252L-.005 10l5.341-4.748a1 1 0 011.328 1.495L3.005 10l3.378 3.002a1 1 0 01-.664 1.748zm8.945-.002L20.005 10l-5.06-4.498a.999.999 0 10-1.328 1.495L16.995 10l-3.659 3.252a1 1 0 001.328 1.496zm-4.678 1.417l2-12a1 1 0 10-1.972-.329l-2 12a1 1 0 101.972.329z"/></svg></a>
                 </p>
